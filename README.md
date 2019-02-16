1e5d4de93cafb90ae0a89788b231e2534c8861ca
SunDoge <384813529@qq.com>
2019-02-16 17:27:46 +0800

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # torch.rs
+
+## Building
+
+### Prerequisites
+
+Install:
+
+- CUDA (need headers)
+- anaconda or miniconda
+- `conda install pytorch torchvision cudatoolkit=YOUR_CUDA_VERSION -c pytorch` (you can install them in envs)
+
+Then you have to set:
+
+- CUDA_PATH
+- TORCH_PATH
+
+e.g.
+
+```bash
+export CUDA_PATH="/usr/local/cuda"
+export TORCH_PATH=/home/sundoge/miniconda3/envs/pytorch1.0/lib/python3.6/site-packages/torch
+```
--- torch-rs/build.rs
+++ torch-rs/build.rs
@@ -2,11 +2,11 @@ use std::env;
 
 fn main() {
     let torch_path = env::var("TORCH_PATH").expect("TORCH_PATH not defined");
-    println!(
-        "cargo:rustc-link-search=native={}/lib/../../../../",
-        torch_path
-    );
-    println!("cargo:rustc-link-search=native={}/lib", torch_path);
+    // println!(
+    //     "cargo:rustc-link-search=native={}/lib/../../../../",
+    //     torch_path
+    // );
+    // println!("cargo:rustc-link-search=native={}/lib", torch_path);
     println!(
         "cargo:rustc-env=LD_LIBRARY_PATH={}/lib:{}/lib/../../../../",
         torch_path, torch_path
